074cbdab59c8852d6175dd72ff31e69cc9cf9916
Hakkar <zugrutus@gmail.com>
2024-03-28 14:04:57 +0000

    added color palette, updated layout for weather page, added reusable stat cards

---
+++ components/InformationPanel.tsx
@@ -0,0 +1,19 @@
+// 'use client'
+
+// import { MoonIcon, SunIcon } from "@heroicons/react/solid"
+// import Image from "next/image";
+// import CityPicker from "./CityPicker";
+
+// type Props = {
+//     city: string;
+//     results: Root;
+//     lat: string;
+//     long: string;
+// }
+// function InformationPanel({city, lat, long, results}: Props) {
+//   return (
+//     <div>InformationPanel</div>
+//   )
+// }
+
+// export default InformationPanel
