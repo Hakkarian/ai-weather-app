fc47673e9353d230547f4270b28083461ac24b03
Hakkar <zugrutus@gmail.com>
2024-03-31 19:11:41 +0100

    styled main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,44 @@
 
 import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
+import { Tilt } from "react-tilt";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-[#394f68] to-[#000000] p-10 flex flex-col justify-center items-center">
-      <Card className="bg-gradient-to-br from-[#394f68] to-[#ffffff] rounded-lg max-w-4xl mx-auto">
-        <h1 className="text-6xl text-center mb-10 font-bold text-gray-600">Wazzer</h1>
-        <Subtitle className="text-xl text-center">Pick up any country, city, and we will catch the weather for you!</Subtitle>
+    <main className="min-h-screen bg-gradient-to-r from-red-500 via-orange-500 via-yellow-500  to-blue-700 p-10 flex flex-col justify-center items-center">
+      <Tilt>
+        <Card className=" rounded-lg max-w-4xl mx-auto bg-opacity-50 bg-clip-padding backdrop-filter backdrop-blur-md">
+          <ul className="flex text-6xl font-bold justify-center mb-10">
+            <li>
+              <h1 className="text-red-500">W</h1>
+            </li>
+            <li>
+              <h1 className="text-orange-500">a</h1>
+            </li>
+            <li>
+              <h1 className="text-yellow-500">z</h1>
+            </li>
+            <li>
+              <h1 className="text-yellow-500">z</h1>
+            </li>
+            <li>
+              <h1 className="text-blue-500">e</h1>
+            </li>
+            <li>
+              <h1 className="text-blue-700">r</h1>
+            </li>
+          </ul>
+          <Subtitle className="text-xl text-center">
+            Pick up any country, city, and we will catch the weather for you!
+          </Subtitle>
 
-        <Divider className="mb-10" />
+          <Divider className="mb-10" />
 
-        <Card className="bg-gradient-to-br from-[#394f68] to-[#000000] rounded "><CityPicker /></Card>
-      </Card>
+          <Card className="bg-gradient-to-br from-[#394f68] to-[#000000] rounded ">
+            <CityPicker />
+          </Card>
+        </Card>
+      </Tilt>
     </main>
   );
 }
