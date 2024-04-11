8e3cfdd29ddb407f1e1d6abc098727a33af292dc
Hakkar <zugrutus@gmail.com>
2024-04-11 15:49:58 +0100

    test4

---
+++ lib/getBasePath.ts
@@ -0,0 +1,6 @@
+const getBasePath = () => {
+    const baseUrl = process.env.NODE_ENV === "development" ? 'http://localhost:3000' : `https://${process.env.VERCEL_URL}`
+    return baseUrl;
+}
+
+export default getBasePath;
